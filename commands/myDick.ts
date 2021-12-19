b62159098c1f8c53c62c200720b10413f70f3f1a
dang nguyen bao <proquadi66@gmail.com>
2021-12-19 09:48:01 +0700

    update

--- commands/myDick.ts
+++ commands/myDick.ts
@@ -6,11 +6,13 @@ export default {
     description: 'generate a random size of pee pee', 
 
     callback: ({message, text}) => {
+
+        let peepee = '3'+'='.repeat(Math.round(Math.random()*20))+'D'
         const embed = new MessageEmbed()
         .setDescription(message.author.username)
         .setTitle('the pee pee machine')
         .setFooter("by distiled's bot")
-        .addField('pee pee size is:', '3'+'='.repeat(Math.round(Math.random()*20))+'D')
+        .addField('pee pee size is:', peepee)
 
         return embed
     },
