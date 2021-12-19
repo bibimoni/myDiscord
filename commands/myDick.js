473219cde2fc4ebdbaa309b82f7bb124fb7af219
dang nguyen bao <proquadi66@gmail.com>
2021-12-19 09:45:34 +0700

    update

---
+++ commands/myDick.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+exports.default = {
+    category: 'fun command',
+    description: 'generate a random size of pee pee',
+    callback: ({ message, text }) => {
+        const embed = new discord_js_1.MessageEmbed()
+            .setDescription(message.author.username)
+            .setTitle('the pee pee machine')
+            .setFooter("by distiled's bot")
+            .addField('pee pee size is:', toString());
+        return embed;
+    }
+};
---
+++ commands/myDick.ts
@@ -0,0 +1,17 @@
+import { MessageEmbed } from "discord.js";
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'fun command',
+    description: 'generate a random size of pee pee', 
+
+    callback: ({message, text}) => {
+        const embed = new MessageEmbed()
+        .setDescription(message.author.username)
+        .setTitle('the pee pee machine')
+        .setFooter("by distiled's bot")
+        .addField('pee pee size is:', '3'+'='.repeat(Math.round(Math.random()*20))+'D')
+
+        return embed
+    },
+} as ICommand
