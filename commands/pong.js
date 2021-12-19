8500d7b573e66ebc73bd323b2aa6b210e1c0f600
dang nguyen bao <proquadi66@gmail.com>
2021-12-19 09:24:33 +0700

    update

---
+++ commands/pong.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    category: 'ping pong',
+    description: 'Replies with Bùi dốt',
+    slash: 'both',
+    testOnly: true,
+    callback: ({}) => {
+        return 'Bùi dốt';
+    },
+};
+//!pong
