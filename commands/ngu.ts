c7a077fc372976f57ed016d8e93fb2674bb213a0
dang nguyen bao <proquadi66@gmail.com>
2021-12-19 21:54:44 +0700

    update

--- commands/ngu.ts
+++ commands/ngu.ts
@@ -8,14 +8,20 @@ export default {
     testOnly: true,
 
     callback: ({}) => {
-        let randomValue = Math.round(Math.random()*3+1);
-        while(randomValue >= 4) {
-            randomValue = Math.round(Math.random()*3+1);
+        let randomValue = Math.round(Math.random()*8+1);
+        while(randomValue >= 9) {
+            randomValue = Math.round(Math.random()*8+1);
         }
-        if(randomValue === 1) return 'béo ngu'
-        else if(randomValue === 2) return 'Bùi dốt'
-        else if(randomValue === 3) return 'Pô bẽm'
-
+        switch(randomValue) {
+            case 1 : return 'béo ngu'; break;
+            case 2 : return 'Bùi dốt'; break;
+            case 3 : return 'Pô bẽm'; break;
+            case 4 : return 'Hoàng non'; break;
+            case 5 : return 'Huy ngu'; break;
+            case 6 : return 'Bảo ngu'; break;
+            case 7 : return 'vịt dốt'; break;
+            case 8 : return 'Phúc ngu'; break;          
+        }
     },
 } as ICommand
 //ngu
