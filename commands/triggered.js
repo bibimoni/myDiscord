90b999b7d42fb15c4aaabd33d3098a4911af5338
dang nguyen bao <proquadi66@gmail.com>
2021-12-20 17:02:33 +0700

    update

---
+++ commands/triggered.js
@@ -0,0 +1,19 @@
+const canvacord = require("canvacord");
+const { MessageAttachment } = require("discord.js");
+module.exports = {
+    category: 'fun',
+    description: 'triggered',
+
+    expectedArgs: '<user @>',
+    minArgs: 1,
+    testOnly: true,
+
+    callback: async ({message}) => {
+        const target = message.mentions.members.first();
+        let avatar = target.displayAvatarURL({dynamic: false, format: 'png'});
+        let image = await canvacord.Canvacord.wasted(avatar);
+        let attachment = new MessageAttachment(image, 'image.png');
+        return message.channel.send(attachment);
+
+    }  
+}
