da1229ebb2ddcbca6362030f2fec22b6bfb4df71
dang nguyen bao <proquadi66@gmail.com>
2021-12-20 15:54:58 +0700

    update

---
+++ commands/image.js
@@ -0,0 +1,21 @@
+const img = require('images-scraper');
+
+
+const google = new img({
+    puppeteer: {
+        headless: true,
+
+    }
+})
+
+module.exports = {
+    name: 'image',
+    run : async(Client, message, args) => {
+        const query = args.join(" ");
+        if(!query) return message.channel.send('nhập từ khóa bạn muốn tìm kiếm');
+
+        const results = await google.scrape(query, 1);
+        message.channel.send(results[0].url);
+
+    }
+}
